bbc4e780e219a4ebc064fb8547436c0acf973e3a
bizLsw91 <biz.lsw91@gmail.com>
2025-02-10 20:33:48 +0900

    cors 에 firebase-strorage

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -30,7 +30,7 @@ export default [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:8598','http://127.0.0.1:8598', 'https://cmc-renewal.vercel.app', 'https://221.168.205.41'],
+      origin: ['http://localhost:8598','http://127.0.0.1:8598', 'https://cmc-renewal.vercel.app', 'https://storage.googleapis.com'],
       credentials: true  // ✅ 쿠키 전송 허용
     },
   },
